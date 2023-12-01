df9456c395c4333eb565dcb6f72efaa204a50cf2
LepronlineR <44305771+LepronlineR@users.noreply.github.com>
2023-12-01 14:39:18 -0500

    combine clips through text

--- Project-EVA/src/modules/speech.py
+++ Project-EVA/src/modules/speech.py
@@ -25,28 +25,24 @@ def speech2speech(model):
 def testing():
     import torch
     from TTS.api import TTS
-    from moviepy.editor import ImageSequenceClip, AudioFileClip, CompositeAudioClip
-    from PIL import Image
+    from moviepy.editor import ImageSequenceClip, AudioFileClip, CompositeAudioClip, concatenate_videoclips
     import soundfile as sf
     import numpy as np
+    import yaml
+
+    with open("config.yaml") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
 
     # Get device
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    # List available 🐸TTS models
-    # for s in TTS().list_models():
-    #     print(s)
-
+    # get TTS
     tts = TTS('tts_models/en/ljspeech/vits').to(device)
-    # tts = TTS('./pretrained_vctk.pth').to(device)
-    # tts = TTS('tts_models/en/ljspeech/vits--neon').to(device)
 
-    # text = "Hello world! My name is Zhi Zheng."
-    # text = "My name is Walter Hartwell White"
-    text = "The significance of this paper lies in its efforts to mitigate a fundamental flaw in deep learning models: their vulnerability to adversarial attacks."
-    wav = tts.tts(text=text)
-    print(type(wav))
-    # tts.tts_to_file(text=text, file_path="output.wav")
+    # define vars
+    sampling_rate = config['video_generator']['audio_sampling_rate']
+    fps = config['video_generator']['fps']
+    seconds_per_video = 25 / fps
 
     def get_seconds_in_wav(wav, sampling_rate = 22050):
         # convert to numpy
@@ -55,40 +51,53 @@ def testing():
 
         return len(wav) / sampling_rate
 
-    # define vars
-    sampling_rate = 22050 # sampling rate of the audio
-    fps = 6 # frames per second of the video
-    seconds_per_video = 25 / fps
+    def generate_video(text):
+        video = torch.rand(25, 3, 512, 512)
+        return video
 
-    total_audio_seconds = get_seconds_in_wav(wav)
+    texts = [
+        "Through the rise of image processing through machine learning, adversarial attacks prove to be a threat to the robustness of models.",
+        "This paper examines a novel purification technique that leverages the robustness of diffusion and model attention.",
+        "We apply inpainting to the attention map of a high layer representation of the model in order to effectively target an area to diffuse.",
+        "This provides a more effective way to purify an attacked image by generalizing the dimensions of which the model classifies.",
+        "Through experimentation we provide applicable methods that effectively eliminate adversarial attacks."
+    ]
 
-    video = torch.rand(25, 3, 512, 512)
-    seconds_overall = seconds_per_video
+    # go through every iteration of text
+    clips = []
+    for text in texts:
+        # generate initial video
+        video = generate_video(text)
+        wav = tts.tts(text=text)
+    
+        total_audio_seconds = get_seconds_in_wav(wav)
 
-    while seconds_overall < total_audio_seconds:
-        # generate the video
-        random_video = torch.rand(25, 3, 512, 512)
+        seconds_overall = seconds_per_video
+        while seconds_overall < total_audio_seconds:
+            # generate the video
+            random_video = generate_video(text)
 
-        video = torch.cat((video, random_video), dim=0)
+            video = torch.cat((video, random_video), dim=0)
 
-        seconds_overall += seconds_per_video
+            seconds_overall += seconds_per_video
 
-    print(video.shape)
+        # get audio
+        wav_numpy = np.array(wav)
+        sf.write('audio.wav', wav_numpy, sampling_rate) 
 
-    # pil_images = [Image.fromarray((np_img * 255).astype('uint8')) for np_img in video.permute(0, 2, 3, 1).numpy()]
-    pil_images = [(np_img * 255).astype('uint8') for np_img in video.permute(0, 2, 3, 1).numpy()]
+        # get clip
+        # transform the video to match the sequence of images required for an image sequence clip
+        tranform_video = [(np_img * 255).astype('uint8') for np_img in video.permute(0, 2, 3, 1).numpy()]
+        clip = ImageSequenceClip(tranform_video, fps=fps) 
 
-    # get audio
-    wav_numpy = np.array(wav)
-    sf.write('audio.wav', wav_numpy, sampling_rate) 
+        # add audio to clip
+        audio = AudioFileClip('audio.wav')
+        composite_audio = CompositeAudioClip([audio.set_duration(audio.duration)])
+        clip = clip.set_audio(composite_audio)
 
-    # get clip
-    clip = ImageSequenceClip(pil_images, fps=fps) 
+        # add to all clips
+        clips.append(clip)
 
-    # add audio to clip
-    audio = AudioFileClip('audio.wav')
-    composite_audio = CompositeAudioClip([audio.set_duration(audio.duration)])
-    clip = clip.set_audio(composite_audio)
 
-    # write out
-    clip.write_videofile("output.mp4", codec="libx264")
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile("output.mp4", codec="libx264")
